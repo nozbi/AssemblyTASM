bf625b3934cf6e4d6a5acc6ec172c75f82bc0ea2
nozbi <76044507+nozbi@users.noreply.github.com>
2022-05-31 16:36:30 +0200

    Add files via upload

---
+++ IsEven.asm
@@ -0,0 +1,107 @@
+.model small
+.stack
+
+.data
+    new_line db "",13,10,"$"  ;dont use
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;variables start
+   string1 db "Type x:","$"
+   string2 db " is even","$"
+   string3 db " is odd","$"
+   x dw ?
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;variables end
+
+.code
+
+get_number_from_input:     ;mov NUMBER_VARIABLE,bx(after to store input in VARIABLE)
+xor bx,bx 
+read:
+cmp bx,65535
+ja return                                                                      
+mov ah,7
+int 21h
+xor dx,dx
+mov dl,al
+cmp dl,13
+je return
+cmp dl,'0'
+jl read
+cmp dl,'9'
+jg read
+mov ah,2
+int 21h
+sub dl,'0'
+push dx
+mov ax,bx
+mov cx,10
+mul cx
+pop dx
+add ax,dx
+mov bx,ax
+jmp read
+return:
+ret
+
+print_number: ;mov ax,NUMBER_VARIABLE
+mov cx,0
+loop1:
+mov dx,0
+mov bx,10
+div bx
+push dx
+inc cx
+cmp ax,0
+jnz loop1
+loop2:
+pop dx
+add dl,48
+mov ah,2
+int 21h
+loop loop2
+ret
+
+print_string:   ;mov ax,offset STRING_VARIABLE 
+mov dx,ax 
+mov ax, @data
+mov ds, ax                  
+mov ah, 09h             
+int 21h   
+ret
+
+print_new_line:
+mov dx, offset new_line 
+mov ax, @data
+mov ds, ax                  
+mov ah, 09h             
+int 21h 
+ret
+
+start:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;start 
+   mov ax,offset string1
+   call print_string
+   
+   call get_number_from_input
+   mov x,bx
+   
+   call print_new_line
+   
+   mov ax,x
+   call print_number
+   
+   mov ax,x
+   test ax,1 
+   jz is_even
+   jmp is_odd
+   is_even:
+   mov ax,offset string2
+   call print_string
+   jmp loop_end
+   is_odd:
+   mov ax,offset string3
+   call print_string
+   jmp loop_end
+   loop_end:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;end
+    mov ax, 4C00h          
+    int 21h                 
+end start
